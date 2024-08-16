208abd184df625d58ee5eb405dbb3cc960a4bc27
Anuj <anujshrivastav530@gmail.com>
2024-08-16 11:53:48 +0530

    test 4

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
