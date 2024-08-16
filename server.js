ca83709fdfdd351af6f10f6c72945ab4df7b7342
Anuj <anujshrivastav530@gmail.com>
2024-08-16 11:49:09 +0530

    test 2

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 
 const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
